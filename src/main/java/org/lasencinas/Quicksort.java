cc7dd88110cac87e384d2e11f186242548a38812
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-04 15:08:57 +0200

    Main method almost done.

--- src/main/java/org/lasencinas/Quicksort.java
+++ src/main/java/org/lasencinas/Quicksort.java
@@ -1,9 +1,28 @@
 package org.lasencinas;
 
 public class Quicksort {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
 
-    public static void swap()
+    public static void sort(int array[]) {
+
+        int leftIndex = 0;
+        int rightIndex = array.length - 1;
+        int pivot = array[array.length / 2];
+        int aux = 0;
+
+        while (leftIndex < rightIndex) {
+            while (array[leftIndex] <= pivot && leftIndex < rightIndex) {
+                leftIndex++;
+            }
+            while (array[rightIndex] > pivot) {
+                leftIndex++;
+            }
+            if (leftIndex < rightIndex) {
+                aux = array[leftIndex];
+                array[leftIndex] = array[rightIndex];
+                array[rightIndex] = aux;
+            }
+        }
+        array[leftIndex] = array[rightIndex];
+        array[rightIndex] = pivot;
+    }
 }
