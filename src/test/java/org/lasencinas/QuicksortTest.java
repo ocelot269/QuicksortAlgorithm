4bf0c51b50c9de51c4069b48f91b4e2c3177abe7
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-05 14:26:55 +0200

    2nd test case not passed.

--- src/test/java/org/lasencinas/QuicksortTest.java
+++ src/test/java/org/lasencinas/QuicksortTest.java
@@ -6,4 +6,19 @@ import org.junit.Test;
 
 public class QuicksortTest {
 
+    private int[] array = {1, 5, 89, 2, 0};
+    private int[] array2 = {4, 5, 78, 23, 32, 11, 9};
+
+    @Test
+    public void quicksortTest() {
+
+        int[] array1Result = {0, 1, 2, 5, 89};
+        int[] array2Result = {4, 5, 9, 11, 23, 32, 78};
+
+        Quicksort.sort(array, 0, array1Result.length - 1);
+        Quicksort.sort(array2, 0, array1Result.length - 1);
+
+        assertArrayEquals(array1Result, array);
+        assertArrayEquals(array2Result, array2);
+    }
 }
