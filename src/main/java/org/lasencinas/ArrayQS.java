e0693b10d26e3f3aa43281b986d22631d107ca7f
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-05-16 19:40:48 +0200

    quicksort implemented with Stack

--- src/main/java/org/lasencinas/ArrayQS.java
+++ src/main/java/org/lasencinas/ArrayQS.java
@@ -1,16 +1,41 @@
 package org.lasencinas;
 
+import java.util.*;
+
 public class ArrayQS {
 
-    public static void sort(int array[], int firstIndex, int lastIndex) {
-
-        Tasca t;
-        t = new Tasca (0, array.lenght - 1);
-        tasques.push(t);
-
-        while(!tasques.isEmpty()) {
-            t = tasques.pop();
-            this.doOneStep(t);
+    /* ---- Behaviours ---- */
+    public static void quicksort(List<Integer> integerList, Stack<Task> stack) {
+        if (!stack.isEmpty()) {
+            Task task = new Task(0, integerList.size() - 1);
+            stack.push(task);
         }
+        while (!stack.isEmpty()) {
+            Task task = stack.pop();
+            orderOnce(integerList, task);
+        }
+    }
+
+    public static void orderOnce(List<Integer> integerList, Task task) {
+        int pivot = (task.getInitPos() + task.getFinalPos()) / 2;
+        int leftIndex = task.getInitPos();
+        int rightIndex = task.getFinalPos();
+        int aux;
+
+        while (leftIndex < rightIndex) {
+            while (integerList.get(leftIndex) <= integerList.get(pivot) && leftIndex < rightIndex) {
+                leftIndex++;
+            }
+            while (integerList.get(rightIndex) > integerList.get(pivot)) {
+                rightIndex--;
+            }
+            if (leftIndex < rightIndex) {
+                aux = integerList.get(leftIndex);
+                integerList.set(leftIndex, integerList.get(rightIndex));
+                integerList.set(rightIndex, aux);
+            }
+        }
+        integerList.set(leftIndex, integerList.get(rightIndex));
+        integerList.set(rightIndex, pivot);
     }
 }
--- src/test/java/org/lasencinas/ArrayQSTest.java
+++ src/test/java/org/lasencinas/ArrayQSTest.java
@@ -2,23 +2,31 @@ package org.lasencinas;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
 public class ArrayQSTest {
 
-    private int[] array = {1, 5, 89, 2, 0};
-    private int[] array2 = {4, 5, 78, 23, 32, 11, 9};
+    /* ---- Properties ---- */
+    private List<Integer> integerList = null;
+    private Stack<Task> tasks = null;
+
+
+    @Before
+    public void setUp() {
+        integerList = new ArrayList<>(Arrays.asList(3, 5, 13, 65 ,7, 1));
+        tasks = new Stack<>();
+    }
 
     @Test
     public void quicksortTest() {
+        ArrayQS.quicksort(integerList,  tasks);
+        assertEquals(Arrays.asList(1, 3, 5, 7, 13, 65), integerList);
 
-        int[] array1Result = {0, 1, 2, 5, 89};
-        int[] array2Result = {4, 5, 9, 11, 23, 32, 78};
-
-        ArrayQS.sort(array, 0, array1Result.length - 1);
-        ArrayQS.sort(array2, 0, array1Result.length - 1);
-
-        assertArrayEquals(array1Result, array);
-        assertArrayEquals(array2Result, array2);
     }
 }
